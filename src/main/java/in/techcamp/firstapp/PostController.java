0a4cf82b3dc093f35c3d3818a21d537f541773dc
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-11-08 02:09:44 +0900

    リストを表示させる

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class PostController {
     @GetMapping("/hello")
@@ -11,5 +13,16 @@ public class PostController {
         var sampleText = "サンプルテキスト";
         model.addAttribute("sampleText", sampleText);
         return "hello";
+    }
+
+    @GetMapping
+    public String showList(Model model){
+        var postList = List.of(
+                new PostEntity(1, "投稿1"),
+                new PostEntity(2, "投稿2"),
+                new PostEntity(3, "投稿3")
+        );
+        model.addAttribute("postList", postList);
+        return "index";
     }
 }
