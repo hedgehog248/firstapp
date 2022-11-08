62df4dc0fb11032b73e09db2b073d191cda306c1
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-11-09 01:14:11 +0900

    リファクタリング：Lombokの@Dataでゲッター・セッターを自動生成

--- src/main/java/in/techcamp/firstapp/PostEntity.java
+++ src/main/java/in/techcamp/firstapp/PostEntity.java
@@ -1,25 +1,11 @@
 package in.techcamp.firstapp;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @AllArgsConstructor
+@Data
 public class PostEntity {
     private long id;
     private String memo;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getMemo() {
-        return memo;
-    }
-
-    public void setMemo(String memo) {
-        this.memo = memo;
-    }
 }
